4ab2d6760f3e531abd5674b1a926190c5b641cb2
Aliaksandr Mamchyts <al.mamchyts@gmail.com>
2021-04-07 00:03:53 +0300

    feat: implement task03

--- src/03-sort-by-height.js
+++ src/03-sort-by-height.js
@@ -9,8 +9,33 @@
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new Error('Not implemented');
+function sortByHeight(array) {
+  const arr = array;
+  const indexs = [];
+  arr.forEach((i, index) => {
+    if (i === -1) {
+      indexs.push(index);
+    }
+  });
+  for (let i = 0, endI = arr.length - 1; i < endI; i++) {
+    for (let j = 0, endJ = endI - i; j < endJ; j++) {
+      if (arr[j] > arr[j + 1]) {
+        const swap = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = swap;
+      }
+    }
+  }
+  arr.reverse();
+  for (let i = 0; i < indexs.length; i++) {
+    arr.pop();
+  }
+  arr.reverse();
+  for (let i = 0; i < indexs.length; i++) {
+    arr.splice(indexs[i], 0, -1);
+  }
+
+  return arr;
 }
 
 module.exports = sortByHeight;
