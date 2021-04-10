853456eca7be0de8a48905830bc8d7145678c40f
Aliaksandr Mamchyts <al.mamchyts@gmail.com>
2021-04-10 18:32:25 +0300

    feat: implement task11

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,23 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const arr = str.split('');
+  let count = 1;
+  let res = '';
+
+  arr.forEach((i, index) => {
+    if (arr[index + 1] === i) {
+      count += 1;
+    } else if (count > 1) {
+      res = `${res}${count}${i}`;
+      count = 1;
+    } else {
+      res = `${res}${i}`;
+      count = 1;
+    }
+  });
+  return res;
 }
 
 module.exports = encodeLine;
