4d1ba46c1157cfefa0261a69cb67eead23aae195
Aliaksandr Mamchyts <al.mamchyts@gmail.com>
2021-04-10 18:11:45 +0300

    feat: implement task10

--- src/10-find-index.js
+++ src/10-find-index.js
@@ -11,8 +11,22 @@
  * For ([1, 2, 3], 2) should return 1
  *
  */
-function findIndex(/* array, value */) {
-  throw new Error('Not implemented');
+function findIndex(array, value) {
+  const arr = array;
+  let maxIndex = arr.length - 1;
+  let minIndex = 0;
+  const { length } = arr;
+  let index = Math.floor(length / 2);
+  while (arr[index] !== value) {
+    if (arr[index] > value) {
+      maxIndex = index;
+      index = Math.floor(maxIndex / 2);
+    } else if (arr[index] < value) {
+      minIndex = index;
+      index = maxIndex - Math.floor((maxIndex - minIndex) / 2);
+    }
+  }
+  return index;
 }
 
 module.exports = findIndex;
